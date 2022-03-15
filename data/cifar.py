d5ad8e2a04a45bc0608ae9fa80ca5a754f29158a
d1ngn1gefe1 <alanzluo@gmail.com>
2022-03-15 14:43:53 -0700

    cifar100

--- data/cifar.py
+++ data/cifar.py
@@ -1,17 +1,14 @@
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, CIFAR100
 
 from .transforms import get_transforms
-
-
-NUM_CLASS = 10
 
 
 class CIFAR10DataModule(LightningDataModule):
   def __init__(self, cfg):
     super().__init__()
-    cfg.num_classes = NUM_CLASS
+    cfg.num_classes = 10
     self.cfg = cfg
 
   def prepare_data(self):
@@ -41,3 +38,38 @@ class CIFAR10DataModule(LightningDataModule):
     cifar10_test = DataLoader(self.cifar10_val, batch_size=self.cfg.batch_size//num_gpus,
                               num_workers=self.cfg.num_workers, pin_memory=True, drop_last=False)
     return cifar10_test
+
+
+class CIFAR100DataModule(LightningDataModule):
+  def __init__(self, cfg):
+    super().__init__()
+    cfg.num_classes = 100
+    self.cfg = cfg
+
+  def prepare_data(self):
+    CIFAR100(self.cfg.dir_data, train=True, download=True)
+    CIFAR100(self.cfg.dir_data, train=False, download=True)
+
+  def setup(self, stage=None):
+    transforms_train, transforms_val, transforms_test = get_transforms(self.cfg.net, self.cfg.augment)
+    self.cifar100_train = CIFAR100(self.cfg.dir_data, train=True, transform=transforms_train)
+    self.cifar100_val = CIFAR100(self.cfg.dir_data, train=False, transform=transforms_val)
+    self.cifar100_test = CIFAR100(self.cfg.dir_data, train=False, transform=transforms_test)
+
+  def train_dataloader(self):
+    num_gpus = len(self.cfg.gpus)
+    cifar100_train = DataLoader(self.cifar100_train, batch_size=self.cfg.batch_size//num_gpus,
+                                num_workers=self.cfg.num_workers, pin_memory=True, drop_last=False)
+    return cifar100_train
+
+  def val_dataloader(self):
+    num_gpus = len(self.cfg.gpus)
+    cifar100_val = DataLoader(self.cifar100_val, batch_size=self.cfg.batch_size//num_gpus,
+                              num_workers=self.cfg.num_workers, pin_memory=True, drop_last=False)
+    return cifar100_val
+
+  def test_loader(self):
+    num_gpus = len(self.cfg.gpus)
+    cifar100_test = DataLoader(self.cifar100_val, batch_size=self.cfg.batch_size//num_gpus,
+                               num_workers=self.cfg.num_workers, pin_memory=True, drop_last=False)
+    return cifar100_test
--- data/get_data.py
+++ data/get_data.py
@@ -1,10 +1,12 @@
-from .cifar import CIFAR10DataModule
+from .cifar import CIFAR10DataModule, CIFAR100DataModule
 from .dp import make_private
 
 
 def get_data(cfg):
   if cfg.dataset == 'cifar10':
     data = CIFAR10DataModule(cfg)
+  elif cfg.dataset == 'cifar100':
+    data = CIFAR100DataModule(cfg)
   else:
     raise NotImplementedError
 
