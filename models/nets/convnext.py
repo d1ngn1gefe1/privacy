convnext per sample gradient with bug

models/nets/convnext.py
@@ -1,10 +1,17 @@
+"""
+Reference:
+ - https://github.com/facebookresearch/ConvNeXt
+ - https://github.com/pytorch/opacus/blob/main/opacus/grad_sample/layer_norm.py#L27
+ - https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/normalization.py#L188
+"""
+
 from functools import partial
-from opacus.utils.tensor_utils import sum_over_all_but_batch_and_last_n
 from opacus.grad_sample import register_grad_sampler
+from opacus.utils.tensor_utils import sum_over_all_but_batch_and_last_n
 import timm
 from timm.models import convnext
-from timm.models.convnext import LayerNorm2d
-from timm.models.layers import trunc_normal_, ClassifierHead, SelectAdaptivePool2d, DropPath, ConvMlp, Mlp
+from timm.models.convnext import LayerNorm2d, _is_contiguous
+from timm.models.layers import DropPath, ConvMlp, Mlp
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,20 +19,32 @@ from typing import Dict
 
 
 @register_grad_sampler(LayerNorm2d)
-def compute_layer_norm_grad_sample(
+def compute_layer_norm_2d_grad_sample(
     layer: LayerNorm2d,
     activations: torch.Tensor,
     backprops: torch.Tensor,
 ) -> Dict[nn.Parameter, torch.Tensor]:
-  # fixme
+  # if _is_contiguous(activations):
+  x = F.layer_norm(activations.permute(0, 2, 3, 1), layer.normalized_shape, layer.weight, layer.bias,
+                   layer.eps).permute(0, 3, 1, 2)
+  ret1 = {
+    layer.weight: sum_over_all_but_batch_and_last_n(x*backprops, layer.weight.dim()),
+    layer.bias: sum_over_all_but_batch_and_last_n(backprops, layer.bias.dim())
+  }
+  # else:
   N, C, H, W = activations.shape
   temp = activations.permute(0, 2, 3, 1).reshape(-1, C)
   x_norm = F.layer_norm(temp, layer.normalized_shape, eps=layer.eps)
   x_norm = torch.sum(x_norm.reshape(N, H, W, C), dim=(1, 2))
-  return {
-    layer.weight: x_norm * torch.sum(backprops, dim=(2, 3)),
+  ret2 = {
+    layer.weight: x_norm*torch.sum(backprops, dim=(2, 3)),
     layer.bias: torch.sum(backprops, dim=(2, 3))
   }
+
+  print('register', ret1[layer.weight].shape, ret2[layer.weight].shape, ret1[layer.bias].shape, ret2[layer.bias].shape)
+  assert False
+
+  return ret1
 
 
 class GammaEmbed(nn.Module):