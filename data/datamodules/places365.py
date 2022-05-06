5ab5c9ad4dbbf7cd7dd7d4852e988d6b170472ac
d1ngn1gefe1 <alanzluo@gmail.com>
2022-05-06 12:20:44 -0700

    small fix

--- data/datamodules/places365.py
+++ data/datamodules/places365.py
@@ -15,9 +15,12 @@ class Places365DataModule(BaseDataModule):
 
   def prepare_data(self):
     dir_places365 = osp.join(self.cfg.dir_data, 'places365')
+    dir_train = osp.join(dir_places365, 'data_large_standard')
+    dir_val = osp.join(dir_places365, 'val_large')
+
     os.makedirs(dir_places365, exist_ok=True)
-    Places365(dir_places365, split='train-standard', download=not osp.exists(osp.join(dir_places365, 'data_large_standard')))
-    Places365(dir_places365, split='val', download=not osp.exists(osp.join(dir_places365, 'val_large')))
+    Places365(dir_places365, split='train-standard', download=not osp.exists(dir_train))
+    Places365(dir_places365, split='val', download=not osp.exists(dir_val))
 
   def setup(self, stage=None):
     dir_places365 = osp.join(self.cfg.dir_data, 'places365')
