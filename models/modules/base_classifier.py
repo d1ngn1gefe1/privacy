b5692671aaa13de4080172ea7b0cc189b0217e84
d1ngn1gefe1 <alanzluo@gmail.com>
2022-05-14 22:25:34 -0700

    add sparse tuning; refactor

--- models/modules/base_classifier.py
+++ models/modules/base_classifier.py
@@ -1,14 +1,9 @@
-from functools import partial
 from pytorch_lightning import LightningModule
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.optim import SGD, AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
-import torchmetrics
 
 from models.nets import get_net
-from .misc import MaskedAUROC, multilabel_loss_with_uncertainty
+from .misc import set_mode, set_task
 
 
 class BaseClassifierModule(LightningModule):
@@ -17,30 +12,8 @@ class BaseClassifierModule(LightningModule):
     self.cfg = cfg
     self.net = get_net(cfg)
 
-    # set trainable parameters
-    if cfg.mode == 'linear_probing' or cfg.mode == 'adapter':
-      for param in self.net.parameters():
-        param.requires_grad = False
-      for param in self.net.get_classifier().parameters():
-        param.requires_grad = True
-
-    # set task-specific variables
-    if cfg.task == 'multi-class':
-      self.get_loss = F.cross_entropy
-      self.get_pred = partial(F.softmax, dim=-1)  # avoid using lambda because it cannot be pickled
-      self.metrics = nn.ModuleDict({'acc1': torchmetrics.Accuracy(average='micro', top_k=1),
-                                    'acc5': torchmetrics.Accuracy(average='micro', top_k=5)})
-    elif cfg.task == 'multi-label':
-      if hasattr(cfg, 'uncertainty'):
-        self.get_loss = partial(multilabel_loss_with_uncertainty, uncertainty_approach=cfg.uncertainty)
-        self.metrics = nn.ModuleDict({'roc': MaskedAUROC(num_classes=cfg.num_classes)})
-      else:
-        self.get_loss = F.multilabel_soft_margin_loss
-        self.metrics = nn.ModuleDict({'acc': torchmetrics.Accuracy(average='macro', num_classes=cfg.num_classes),
-                                      'roc': torchmetrics.AUROC(num_classes=cfg.num_classes)})
-      self.get_pred = torch.sigmoid
-    else:
-      raise NotImplementedError
+    set_mode(self.net, cfg)  # set trainable parameters
+    self.get_loss, self.get_pred, self.metrics = set_task(cfg)  # set task-specific variables
 
   def forward(self, x):
     return self.net(x)
