ddbd66d7b61b469b1191079501a2851f23691012
d1ngn1gefe1 <alanzluo@gmail.com>
2022-05-24 21:59:26 -0700

    dp incomplete

--- models/nets/vit.py
+++ models/nets/vit.py
@@ -6,7 +6,7 @@ Examples of register_grad_sampler :
   - https://github.com/pytorch/opacus/blob/main/opacus/grad_sample/dp_multihead_attention.py
 """
 
-import clip
+import clip.model
 from opacus.grad_sample import register_grad_sampler
 import os.path as osp
 import timm
@@ -20,7 +20,7 @@ from typing import Dict
 from .misc import get_norms
 
 
-class ParamEmbed(nn.Module):
+class EmbedTimm(nn.Module):
   def __init__(self, cls_token, pos_embed):
     super().__init__()
     self.cls_token = cls_token
@@ -32,9 +32,9 @@ class ParamEmbed(nn.Module):
     return x
 
 
-@register_grad_sampler(ParamEmbed)
-def compute_param_embed_grad_sample(
-    layer: ParamEmbed, activations: torch.Tensor, backprops: torch.Tensor
+@register_grad_sampler(EmbedTimm)
+def compute_embed_timm_grad_sample(
+    layer: EmbedTimm, activations: torch.Tensor, backprops: torch.Tensor
 ) -> Dict[nn.Parameter, torch.Tensor]:
   ret = {
     layer.pos_embed: backprops.unsqueeze(1),
@@ -43,14 +43,14 @@ def compute_param_embed_grad_sample(
   return ret
 
 
-class VisionTransformer(vision_transformer.VisionTransformer):
+class VisionTransformerTimm(vision_transformer.VisionTransformer):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.param_embed = ParamEmbed(self.cls_token, self.pos_embed)
+    self.embed_timm = EmbedTimm(self.cls_token, self.pos_embed)
 
   def forward_features(self, x):
     x = self.patch_embed(x)
-    x = self.param_embed(x)
+    x = self.embed_timm(x)
     x = self.pos_drop(x)
     if self.grad_checkpointing and not torch.jit.is_scripting():
       x = checkpoint_seq(self.blocks, x)
@@ -61,45 +61,79 @@ class VisionTransformer(vision_transformer.VisionTransformer):
 
   @torch.jit.ignore
   def no_weight_decay(self):
-    return {'param_embed.pos_embed', 'param_embed.cls_token'}
+    return {'embed_timm.pos_embed', 'embed_timm.cls_token'}
 
 
-def delattrs(net):
+def delattrs_timm(net):
   delattr(net, 'cls_token')
   delattr(net, 'pos_embed')
 
 
+class EmbedCLIP(nn.Module):
+  def __init__(self, class_embedding, positional_embedding):
+    super().__init__()
+    self.class_embedding = class_embedding
+    self.positional_embedding = positional_embedding
+
+  def forward(self, x):
+    x = torch.cat([self.class_embedding+torch.zeros(x.shape[0], 1, x.shape[-1], device=x.device), x], dim=1)
+    x = x+self.positional_embedding
+
+    return x
+
+
+@register_grad_sampler(EmbedCLIP)
+def compute_embed_clip_grad_sample(
+    layer: EmbedCLIP, activations: torch.Tensor, backprops: torch.Tensor
+) -> Dict[nn.Parameter, torch.Tensor]:
+  ret = {
+    layer.positional_embedding: backprops.unsqueeze(1),
+    layer.class_embedding: backprops[:, 0].unsqueeze(1).unsqueeze(2)  # TODO
+  }
+  return ret
+
+
 class VisionTransformerCLIP(nn.Module):
-  def __init__(self, num_classes):
-    super(VisionTransformerCLIP, self).__init__()
-    net, _ = clip.load('ViT-B/32')
-    net.proj = nn.Linear(in_features=512, out_features=num_classes, bias=True)
+  def __init__(self, net, num_classes):
+    super().__init__()
     self.visual = net.visual
+    self.embed_clip = EmbedCLIP(self.visual.class_embedding, self.visual.positional_embedding)
+    self.proj = nn.Linear(in_features=768, out_features=num_classes, bias=True)
 
-  def forward(self, images):
-    return self.visual(images)
+    delattr(self.visual, 'class_embedding')
+    delattr(self.visual, 'positional_embedding')
+    delattr(self.visual, 'proj')
 
-  def get_classifier(self):
-    return self.visual.proj
+  def forward(self, x: torch.Tensor):
+    x = self.visual.conv1(x)
+    x = x.reshape(x.shape[0], x.shape[1], -1)
+    x = x.permute(0, 2, 1)
+    x = self.embed_clip(x)
+    x = self.visual.ln_pre(x)
 
+    x = x.permute(1, 0, 2)  # NLD -> LND
+    x = self.visual.transformer(x)
+    x = x.permute(1, 0, 2)  # LND -> NLD
 
-def convert_weights(model):
-  pass
+    x = self.visual.ln_post(x[:, 0, :])
+
+    x = self.proj(x)
+    return x
 
 
 def get_vit(cfg):
   # vit_small_patch16_224: in21k -> in1k, 21.7M parameters
   if cfg.mode == 'from_scratch':
     print('Initializing randomly')
-    vision_transformer.VisionTransformer = VisionTransformer
+    vision_transformer.VisionTransformer = VisionTransformerTimm
     net = timm.create_model('vit_small_patch16_224', pretrained=False, num_classes=cfg.num_classes)
-    delattrs(net)
+    delattrs_timm(net)
 
   elif cfg.weight == 'ckpt':
     print('Loading checkpoint')
-    vision_transformer.VisionTransformer = VisionTransformer
+    vision_transformer.VisionTransformer = VisionTransformerTimm
     net = timm.create_model('vit_small_patch16_224', pretrained=False, num_classes=cfg.num_classes)
-    delattrs(net)
+    delattrs_timm(net)
 
     weight = torch.load(osp.join(cfg.dir_weights, cfg.rpath_ckpt))['state_dict']
     weight = {k.removeprefix('net.'):v for k, v in weight.items()}
@@ -110,16 +144,18 @@ def get_vit(cfg):
     print(f'{keys_missing} will be trained from scratch')
 
   elif cfg.weight == 'pretrain':
-    vision_transformer.VisionTransformer = VisionTransformer
+    vision_transformer.VisionTransformer = VisionTransformerTimm
     print('Loading ImageNet pre-trained weight (timm)')
     net = timm.create_model('vit_small_patch16_224', pretrained=True, num_classes=cfg.num_classes)
-    delattrs(net)
+    delattrs_timm(net)
 
   elif cfg.weight == 'pretrain_clip':
     print('Loading ImageNet pre-trained weight (CLIP)')
-    import clip.model
-    clip.model.convert_weights = convert_weights
-    net = VisionTransformerCLIP(cfg.num_classes)
+
+    clip.model.convert_weights = lambda model: None
+    net, _ = clip.load('ViT-B/16')
+    net = VisionTransformerCLIP(net, cfg.num_classes)
+
   else:
     raise NotImplementedError
 
